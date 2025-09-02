a81773ac4aa4e7fa5cf20995ff8b5d3c4534715e
Jonatan <jonreabarbosa@gmail.com>
2025-09-02 16:20:14 -0600

    Feat: Implementar la funcionalidad de compra y escaneo de tickets con validación y QRcode generation

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import helnet from 'helmet';
+import helmet from 'helmet';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import routes from './routes/index.js'
+import routes from './routes/index.js';
 import { env } from './config/env.js';
-//import { errorHandler, notFoundHandler } from './middlewares/error.js';
+import { errorHandler, notFoundHandler } from './middlewares/error.js';
 
 export function buildApp() {
     const app = express();
-    app.use(cors({origin: env.corsOrigin, credentials: true}));
-    app.use(express.json({limit: '2mb'}));
+    app.use(helmet());
+    app.use(cors({ origin: env.corsOrigin, credentials: true }));
+    app.use(express.json({ limit: '2mb' }));
     app.use(cookieParser());
-    app.use(morgan(env.nodeEnv === 'production' ? 'conbined' : 'dev'))};
+    app.use(morgan(env.nodeEnv === 'production' ? 'combined' : 'dev'));
 
-    app.get('/healt', (_req, res) => res.json({ok: true}));
-    app.use('/api', routers);
-    //app.use(noyFoundHandler);
-    //app.use(errorHandler);
+    app.get('/health', (_req, res) => res.json({ ok: true }));
+    app.use('/api', routes);
+    app.use(notFoundHandler);
+    app.use(errorHandler);
     return app;
+}
